fe40096ae0a1c4b523a7c074a9ecdce5b7de520e
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2025-11-23 14:24:39 +0000

    jdfjk

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialize a square with a private size attribute"""
+        self.__size = size
